f86c4406d76a0e63262f230121b22067b6956c08
scottf01 <59808821+scottf01@users.noreply.github.com>
2020-03-04 20:19:21 -0500

    Update my_timer.c

--- Part 2/my_timer.c
+++ Part 2/my_timer.c
@@ -16,9 +16,9 @@ MODULE_DESCRIPTION("Display the value of my_timer on each proc read.");
 #define MODULE_PERMISSIONS 0644
 #define MODULE_PARENT NULL
 #define STRING_LENGTH 256
-#define MALLOC_FLAGS __GFP_RECLAIM | __GFP_IO | __GFP_FS
+#define MALLOC_FLAGS "__GFP_RECLAIM | __GFP_IO | __GFP_FS"
 
-static struct file_operations operations; // Points to proc file definitions.
+static struct file_operations ops; // Points to proc file definitions.
 static char *string1, *string2;
 static int readProc;
 static int procReadNumber = 0;
@@ -26,29 +26,31 @@ struct timespec currentTime;
 long elapsedSecs;
 long elapsed_ns;
 
-
+//Function declarations
 static int timer_module_init(void);
-int timer_proc_open(struct inode *sp_inode, struct file *sp_file);
-ssize_t timer_read_proc(struct file *sp_file, char __user*buffer, size_t size, loff_t *offset);
-int timer_proc_release(struct inode *sp_inode, struct file *sp_file);
+int timer_proc_open(struct inode *sp_inode, struct file *structfile);
+ssize_t timer_read_proc(struct file *file1, char __user*buffer, size_t size, loff_t *offset);
+int timer_proc_release(struct inode *inode1, struct file *file2);
 unsigned long copy_to_user(void __user *to,const void *from, unsigned long size);
 static void timer_module_exit(void);
 
+//time structure
 static struct time
 {
     long seconds;
     long nanoseconds;
 } time;
 
+//Initialize the module
 static int timer_module_init(void)
 {
     printk(KERN_NOTICE "Creating /proc/%s.\n", MODULE_NAME);
 
-    operations.open = timer_proc_open;
-    operations.read = timer_read_proc;
-    operations.release = timer_proc_release;
+    ops.open = timer_proc_open;
+    ops.read = timer_read_proc;
+    ops.release = timer_proc_release;
 
-    if (!proc_create(MODULE_NAME, MODULE_PERMISSIONS, MODULE_PARENT, &operations))
+    if (!proc_create(MODULE_NAME, MODULE_PERMISSIONS, MODULE_PARENT, &ops))
     {
         printk(KERN_WARNING "Error: could not create proc entry.\n");
         remove_proc_entry(MODULE_NAME, MODULE_PARENT);
@@ -59,28 +61,30 @@ static int timer_module_init(void)
     return 0;
 }
 
-int timer_proc_open(struct inode *sp_inode, struct file *sp_file)
+
+//OPEN THE PROC
+int timer_proc_open(struct inode *sp_inode, struct file *structfile)
 {
     printk(KERN_INFO "timer_proc_open() called.\n");
     readProc = 1;
 
-    procReadNumber++;
+    procReadNumber++;		//Increase number for display purposes
 
     string1 = kmalloc(sizeof(char) * STRING_LENGTH, MALLOC_FLAGS);
     string2 = kmalloc(sizeof(char) * STRING_LENGTH, MALLOC_FLAGS);
     if (string1 == NULL || string2 == NULL) {
-        printk(KERN_WARNING "Error: could not allocate memory for a string.");
+        printk(KERN_WARNING "Error: could not allocate memory.");
 
         return -ENOMEM;
     }
 
     currentTime = current_kernel_time();
-    sprintf(string1, "Current time: %ld.%ld\n", currentTime.tv_sec, currentTime.tv_nsec);
+    sprintf(string1, "current time: %ld.%ld\n", currentTime.tv_sec, currentTime.tv_nsec);
 
-    if(currentTime.tv_nsec - time.nanoseconds < 0)
+    if(currentTime.tv_nsec - time.nanoseconds < 0)		//Math to compensate for sec/ns values
     {
         elapsedSecs = currentTime.tv_sec - time.seconds - 1;
-        elapsed_ns = currentTime.tv_nsec - time.nanoseconds + 1000000000;
+        elapsed_ns = currentTime.tv_nsec - time.nanoseconds + 1000000000;	//"adds one second's time"
     }
     else
     {
@@ -88,9 +92,9 @@ int timer_proc_open(struct inode *sp_inode, struct file *sp_file)
         elapsed_ns = currentTime.tv_nsec - time.nanoseconds;
     }
 
-    if (procReadNumber > 1)
+    if (procReadNumber > 1)		//If it's not the first read, display elapsed time.
     {
-        sprintf(string2, "Elapsed time: %ld.%ld\n", elapsedSecs, elapsed_ns);
+        sprintf(string2, "elapsed time: %ld.%ld\n", elapsedSecs, elapsed_ns);
         strcat(string1, string2);
     }
 
@@ -100,7 +104,7 @@ int timer_proc_open(struct inode *sp_inode, struct file *sp_file)
     return 0;
 }
 
-ssize_t timer_read_proc(struct file *sp_file, char __user
+ssize_t timer_read_proc(struct file *file1, char __user
 
 *buffer, size_t size, loff_t *offset)
 {
@@ -112,15 +116,15 @@ if (readProc == 1)
 return 0;
 }
 
-printk(KERN_INFO "ReadProc() called.\n");
+printk(KERN_INFO "timer_read_proc() called.\n");
 copy_to_user(buffer, string1, length);
 
 return length;
 }
 
-int timer_proc_release(struct inode *sp_inode, struct file *sp_file)
+int timer_proc_release(struct inode *inode1, struct file *file2)
 {
-    printk(KERN_NOTICE "ReleaseProc() has been called.\n");
+    printk(KERN_NOTICE "timer_release_proc() has been called.\n");
     kfree(string1);
     kfree(string2);
 
